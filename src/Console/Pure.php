85d33846d329d9739459f8c3d032523b0407e75f
Lawrence <media@absalom.biz>
2016-07-01 13:07:01 +1000

    Pure console build sequence

---
+++ src/Console/Pure.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Yab\Laracogs\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Yab\Laracogs\Generators\FileMakerTrait;
+
+class Pure extends Command
+{
+    use FileMakerTrait;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'laracogs:pure';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Laracogs will add PureCSS framework to your app (no Bootstrap overhead)';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (!file_exists(base_path('resources/views/team/create.blade.php'))) {
+            $this->line("\n\nPlease perform the starter command:\n");
+            $this->info("\n\nphp artisan laracogs:starter\n");
+            $this->line("\n\nThen one you're able to run the unit tests successfully re-run this command, to make your app run PureCSS:)\n");
+        } else {
+            $fileSystem = new Filesystem();
+
+            $files = $fileSystem->allFiles(__DIR__.'/../Packages/Pure');
+            $this->line("\n");
+            foreach ($files as $file) {
+                $this->line(str_replace(__DIR__.'/../Packages/Pure/', '', $file));
+            }
+
+            $this->info("\n\nThese files will be published\n");
+
+            $result = $this->confirm('Are you sure you want to overwrite any files of the same name?');
+
+            if ($result) {
+                $this->copyPreparedFiles(__DIR__.'/../Packages/Pure/', base_path());
+
+                $this->info("\n\nMake sure you set the PagesController@dashboard to use the following view: 'dashboard.main'\n\n");
+                $this->info("Run the following:\n");
+                $this->comment("gulp\n");
+                $this->info("Finished making your app run PureCSS\n");
+            } else {
+                $this->info('You cancelled the laracogs pure install');
+            }
+        }
+    }
+}
