aa55ec58aad54be15a05248cdffd8ef0622e4123
Lawrence <media@absalom.biz>
2016-07-04 15:01:29 +1000

    Better user admin layout

--- src/Packages/Pure/resources/views/admin/users/index.blade.php
+++ src/Packages/Pure/resources/views/admin/users/index.blade.php
@@ -2,47 +2,58 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-12">
-            <form id="" class="pull-right raw-margin-top-24 raw-margin-left-24" method="post" action="/admin/users/search">
-                {!! csrf_field() !!}
-                <input class="form-control" name="search" placeholder="Search">
-            </form>
-            <a class="btn btn-default pull-right raw-margin-top-24" href="{{ url('admin/users/invite') }}">Invite New User</a>
+        <div class="header">
             <h1>User Admin</h1>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <table class="table table-striped raw-margin-top-24">
+
+        <div class="content">
+                <div class="pure-g">
+                        <div class="pure-u-1 pure-u-md-1-2">
+                            {!! Form::open(['url' => '/admin/users/search', 'class' => '']) !!}
+                            <div class="field">
+                                <input class="fluid" name="search" placeholder="Search">
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
+                        <div class="pure-u-1 pure-u-md-1-2">
+                            <a class="pure-button pure-button-primary" href="{{ url('admin/users/invite') }}">Invite New User</a>
+                        </div>
+                </div>
+
+                <p>
+                @if ($users->isEmpty())
+                        <div class="well center aligned">No users found.</div>
+                @else
+
+                <table class="pure-table pure-table-horizontal">
 
                 <thead>
+                    <th>Name</th>
                     <th>Email</th>
-                    <th class="text-right">Actions</th>
+                    <th>Actions</th>
                 </thead>
                 <tbody>
                     @foreach($users as $user)
-
-                        @if ($user->id !== Auth::id())
                             <tr>
+                                <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>
                                     <form method="post" action="{!! url('admin/users/'.$user->id) !!}">
                                         {!! csrf_field() !!}
                                         {!! method_field('DELETE') !!}
-                                        <button class="btn btn-danger btn-xs pull-right" type="submit" onclick="return confirm('Are you sure you want to delete this user?')"><i class="fa fa-trash"></i> Delete</button>
+                                        <button class="pure-button button-warning button-small" type="submit" onclick="return confirm('Are you sure you want to delete this user?')"><i class="fa fa-trash"></i> Delete</button>
                                     </form>
-                                    <a class="btn btn-warning btn-xs pull-right raw-margin-right-16" href="{{ url('admin/users/'.$user->id.'/edit') }}"><span class="fa fa-edit"></span> Edit</a>
+                                    <a class="pure-button pure-button-primary button-small" href="{{ url('admin/users/'.$user->id.'/edit') }}"><span class="fa fa-edit"></span> Edit</a>
                                 </td>
                             </tr>
-                        @endif
 
                     @endforeach
 
                 </tbody>
 
             </table>
-        </div>
+            @endif
+        </p>
     </div>
 
 @stop
