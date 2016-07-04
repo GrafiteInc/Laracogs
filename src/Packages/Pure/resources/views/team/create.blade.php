fe0636a057df487cd95278f93e82d16e223dd6be
Lawrence <media@absalom.biz>
2016-07-04 13:11:12 +1000

    More form work

--- src/Packages/Pure/resources/views/team/create.blade.php
+++ src/Packages/Pure/resources/views/team/create.blade.php
@@ -5,24 +5,17 @@
         <div class="header">
             <h1>Team Creator</h1>
         </div>
-<div class="content">
+        <div class="content">
             @include('partials.errors')
             @include('partials.message')
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-4 col-md-offset-4">
-                {!! Form::open(['route' => 'teams.store']) !!}
+             {!! Form::open(['route' => 'teams.store', 'class' => 'pure-form pure-form-aligned']) !!}
 
                 @form_maker_table("teams", ['name' => 'string'])
 
-                {!! Form::submit('Save', ['class' => 'ure-button pure-button-primary']) !!}
+                {!! Form::submit('Save', ['class' => 'pure-button pure-button-primary']) !!}
 
                 {!! Form::close() !!}
-            </div>
-
         </div>
-    </div>
+
 
 @stop
